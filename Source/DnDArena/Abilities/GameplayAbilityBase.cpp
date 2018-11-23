af1da5c13eba63331d892333fe94cca4742ca046
Jason Allen <j8allen@uwaterloo.ca>
2018-11-22 23:29:21 -0500

    Nov 22 Code

--- Source/DnDArena/Abilities/GameplayAbilityBase.cpp
+++ Source/DnDArena/Abilities/GameplayAbilityBase.cpp
@@ -1,6 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "AbilityActorBase.h"
 #include "GameplayAbilityBase.h"
+
+void UGameplayAbilityBase::ExecutionLogic()
+{		
+	if (DoesAbilityTagsContain(FGameplayTag::RequestGameplayTag(FName("Ability.Projectile"))))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *GetAbilityActorSpawnLocation().ToString());
+		SpawnProjectile();
+	}
+}
+
+void UGameplayAbilityBase::SpawnProjectile_Implementation()
+{
+	UClass* ClassName = AbilityActor->GeneratedClass;
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	GetWorld()->SpawnActor<AAbilityActorBase>(ClassName, GetAbilityActorSpawnLocation(), GetAbilityActorSpawnRotation(), SpawnParams);
+}
+
+bool UGameplayAbilityBase::SpawnProjectile_Validate()
+{
+	return true;
+}
+
+FVector UGameplayAbilityBase::GetAbilityActorSpawnLocation()
+{
+	return GetAvatarActorFromActorInfo()->GetActorLocation();
+}
+
+FRotator UGameplayAbilityBase::GetAbilityActorSpawnRotation()
+{
+	return GetAvatarActorFromActorInfo()->GetActorRotation();
+}
 
 bool UGameplayAbilityBase::DoesAbilityTagsContain(FGameplayTag Tag) const
 {
--- Source/DnDArena/Abilities/GameplayAbilityBase.h
+++ Source/DnDArena/Abilities/GameplayAbilityBase.h
@@ -6,6 +6,8 @@
 #include "Abilities/GameplayAbility.h"
 #include "GameplayAbilityBase.generated.h"
 
+class AAbilityActorBase;
+
 /**
  * 
  */
@@ -13,11 +15,32 @@ UCLASS()
 class DNDARENA_API UGameplayAbilityBase : public UGameplayAbility
 {
 	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animation, meta = (AllowPrivateAccess = "true"))
+		UAnimMontage* AbilityAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Execution, meta = (AllowPrivateAccess = "true"))
+		UBlueprint* AbilityActor;
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = Ability)
+		void ExecutionLogic();
+
+	UFUNCTION(Server, Reliable, WithValidation, Category = Ability)
+		void SpawnProjectile();
+
+	UFUNCTION()
+		FVector GetAbilityActorSpawnLocation();
+
+	UFUNCTION()
+		FRotator GetAbilityActorSpawnRotation();
 	
 public:
 	virtual uint8 GetGameplayTaskDefaultPriority() const override { return FGameplayTasks::DefaultPriority; }
 	
 	UFUNCTION(BlueprintCallable, Category = Ability)
 		bool DoesAbilityTagsContain(FGameplayTag Tag) const;
+
 	
 };
